108ab248bcdbab7d100114df6b7b2949970c2fe1
kylejaster <kyle@noodle.org>
2015-03-03 22:30:25 -0500

    rebuilding site Tue Mar  3 22:30:25 EST 2015

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:kylejaster/agrarianist.git gh-pages
+git subtree push --force --prefix=public git@github.com:kylejaster/agrarianist.git gh-pages
